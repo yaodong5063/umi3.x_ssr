322678c9b6593f44b2e3b47201e8fcf054a2658f
yaodongdong <yaodongdong@qbao.com>
2020-08-31 21:08:34 +0800

    '部分丢失文件上传'

---
+++ .umirc.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  layout:false,
+  //routes不配置，为约定式路由，可根据pages里目录自动生成
+  // routes: [
+  //   { path: '/', component: '@/pages/index' },
+  // ],
+  plugins:[],
+  proxy: {
+    '/API_HOST': {
+        target: 'https://crm.paat.com',
+        changeOrigin: true,
+        pathRewrite: { '^/API_HOST': '' },
+        cookieDomainRewrite: {
+            '*': 'localhost',
+        },
+    },
+  },
+  ssr: {
+    // 默认为 true
+    devServerRender: false,
+  },
+  theme: {
+    // 'primary-color':'#FFC600'
+  },
+  targets: {
+      ie: 10,
+  },
+  chainWebpack (config, { webpack }) {
+   
+  }
+});
